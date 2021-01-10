f880823870a230ca51abb7e62fab73c4abb5a535
Jeffrey Skinner <17016344+blocksrocks@users.noreply.github.com>
2021-01-09 20:53:35 -0800

    setters and getters

--- Radix.lua
+++ Radix.lua
@@ -1,23 +1,32 @@
 local Radix = {}
 Radix.__index = Radix
 
-function Radix.new(base, digits)
+function Radix.new(base)
 	local self = setmetatable({}, Radix)
 
 	self._base = base or 10
-	self._digits = digits or {}
+	self._digits = {}
 
 	return self
 end
 
+function Radix:GetBase()
+	return self._base
+end
+
 function Radix:SetBase(base)
 	local decimal = self:GetDecimal()
+
 	self._base = base
 	self:SetDecimal(decimal)
 end
 
 function Radix:GetDigits()
 	return self._digits
+end
+
+function Radix:SetDigits(digits)
+	self._digits = digits
 end
 
 function Radix:GetDecimal()
@@ -33,9 +42,13 @@ end
 function Radix:SetDecimal(decimal)
 	self._digits = {}
 
-	while decimal > 0 do
-		table.insert(self._digits, 1, decimal%self._base)
-		decimal = math.floor(decimal/self._base)
+	if decimal > 0 then
+		while decimal > 0 do
+			table.insert(self._digits, 1, decimal%self._base)
+			decimal = math.floor(decimal/self._base)
+		end
+	else
+		table.insert(self._digits, 0)
 	end
 end
 
